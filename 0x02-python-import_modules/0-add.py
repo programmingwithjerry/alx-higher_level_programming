6ad6c436e2a0186cce34fcbdd89a470492c34a5c
programmingwithjerry <eneyellowj@gmail.com>
2024-03-14 13:57:38 +0000

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
-# Importing the add function from the add_0 module
 if __name__ == "__main__":
     from add_0 import add
 
     a = 1
     b = 2
-    # Printing the result of the addition
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
