7eca269fad8bc34acd0d3eb63a46dce6f39b08b3
programmingwithjerry <eneyellowj@gmail.com>
2024-03-20 18:13:15 +0000

    4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,3 @@
+#!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    diff_set_1 = set_1 - set_2
-    diff_set_2 = set_2 - set_1
-    return (diff_set_1.union(diff_set_2))
+    return (set_1 ^ set_2)
