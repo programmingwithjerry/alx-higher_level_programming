0c250258630cdc2df9e45e63e2bab5466a59dd9f
programmingwithjerry <eneyellowj@gmail.com>
2024-04-03 15:37:29 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" This module contains the definition of a class named Square."""
+class Square:
+    """ The Square class."""
+    def __init__(self, size):
+        """ Initialize the class """
+        self.__size = size
