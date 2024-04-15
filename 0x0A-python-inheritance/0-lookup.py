3ccf8729821816eb868859df677b27e8356fdd3d
programmingwithjerry <eneyellowj@gmail.com>
2024-04-15 17:11:57 +0000

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,10 +5,12 @@ Module with one class - MyList
 
 
 class MyList(list):
-    """My list Class"""
+    """Class with method print_sorted"""
     pass
 
     def print_sorted(self):
-        """returns the list of available attributes and methods of an object"""
+        """Returns the list of available
+           attributes and methods of an object
+        """
 
         print(sorted(list(self)))
