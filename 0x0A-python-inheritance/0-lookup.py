8f7ec7fe8de76150a7a58fb84ef7d5324892eee9
programmingwithjerry <eneyellowj@gmail.com>
2024-04-15 17:18:38 +0000

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
+"""module with one funaction named lookup
 """
-Module with one class - MyList
-"""
 
 
-class MyList(list):
-    """Class with method print_sorted"""
-    pass
-
-    def print_sorted(self):
-        """Returns the list of available
-           attributes and methods of an object
-        """
-
-        print(sorted(list(self)))
+def lookup(obj):
+    """returns the list of available attributes and
+       methods of an object
+    """
+    return (list(dir(obj)))
