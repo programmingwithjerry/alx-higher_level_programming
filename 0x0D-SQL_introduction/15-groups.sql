fb22bc2b3a090269a6df1e0f53d86b6d52f05ce1
programmingwithjerry <eneyellowj@gmail.com>
2024-05-22 19:52:59 +0000

    15-groups.sql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,5 @@
--- Grouping - Display the count of records for each score
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score DESC;
+-- Display the count of records for each score in the table second_table
+SELECT `score`, COUNT(*) AS `number`
+FROM `second_table`
+GROUP BY `score`
+ORDER BY `number` DESC;
