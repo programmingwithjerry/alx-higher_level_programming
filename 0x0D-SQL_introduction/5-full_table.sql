6a9f054ae162a068ab69f59e8737fbea543889e6
programmingwithjerry <eneyellowj@gmail.com>
2024-05-22 18:50:21 +0000

    5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,3 @@
 -- Display the complete structure of the table
 -- first_table from the database hbtn_0c_0
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM information_schema.columns
-WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
+SHOW CREATE TABLE `first_table`;
