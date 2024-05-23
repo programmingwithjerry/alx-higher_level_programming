c70c41a53604fc5f2f01bd8f2b5be7ae1bd0bcf8
programmingwithjerry <eneyellowj@gmail.com>
2024-05-23 18:00:08 +0000

    9-cities_by_state_join.sql

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,10 @@
+-- Use the `hbtn_0d_usa` database
+USE hbtn_0d_usa;
+
+-- Select specific columns from `cities` and `states` using a LEFT
+-- JOIN with a NULL check to mimic INNER JOIN behavior
+SELECT cities.id, cities.name, states.name
+FROM cities
+LEFT JOIN states ON cities.state_id = states.id
+WHERE states.id IS NOT NULL;
+
