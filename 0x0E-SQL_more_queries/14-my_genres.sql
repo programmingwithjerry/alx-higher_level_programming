ebee7d8de5b2d33554b8f03960f95b147d1cead9
programmingwithjerry <eneyellowj@gmail.com>
2024-05-23 18:38:56 +0000

    14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,12 +1,9 @@
--- Select distinct genre names using a subquery in the WHERE clause
+-- Select distinct genre names using table aliases
 SELECT DISTINCT g.name
 FROM tv_genres AS g
-WHERE g.id IN (
-    SELECT sg.genre_id
-    FROM tv_show_genres AS sg
-    INNER JOIN tv_shows AS s
-    ON sg.show_id = s.id
-    WHERE s.title = 'Dexter'
-)
+INNER JOIN tv_show_genres AS sg
+ON g.id = sg.genre_id
+INNER JOIN tv_shows AS s
+ON sg.show_id = s.id
+WHERE s.title = 'Dexter'
 ORDER BY g.name ASC;
-
