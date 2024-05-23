ecd36e6b2fd356dfe1e57a0ee20b0340986bcdc1
programmingwithjerry <eneyellowj@gmail.com>
2024-05-23 18:19:21 +0000

    12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,10 @@
--- Select the `title` and `genre_id` using a derived table to filter out shows with genres
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT OUTER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+-- Select the `title` and `genre_id` using NOT EXISTS to check for absence of genres
+SELECT s.title, NULL AS genre_id
+FROM tv_shows AS s
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM tv_show_genres AS g
+    WHERE s.id = g.show_id
+)
+ORDER BY s.title ASC;
+
