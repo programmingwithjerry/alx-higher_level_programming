e5e06a3e48e036768993b1b5e3435754a5f5d699
programmingwithjerry <eneyellowj@gmail.com>
2024-05-23 18:37:33 +0000

    13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,7 @@
--- Select distinct genre names using table aliases
-SELECT DISTINCT g.name
+-- Select the genre and count the number of shows using table aliases
+SELECT g.name AS genre, COUNT(sg.show_id) AS number_of_shows
 FROM tv_genres AS g
 INNER JOIN tv_show_genres AS sg
 ON g.id = sg.genre_id
-INNER JOIN tv_shows AS s
-ON sg.show_id = s.id
-WHERE s.title = 'Dexter'
-ORDER BY g.name ASC;
+GROUP BY sg.genre_id
+ORDER BY number_of_shows DESC;
