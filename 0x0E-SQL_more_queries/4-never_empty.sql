81d6a3f1c165000f61a4f1ba32d795bc7698f5c7
programmingwithjerry <eneyellowj@gmail.com>
2024-05-23 00:17:49 +0000

    4-never_empty.sql

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,7 +1,8 @@
 -- Create the table `id_not_null` if it does not already exist
--- The `name` column is a variable character string of up to 256 characters
 -- The `id` column is an integer with a default value of 1 and cannot be NULL
+-- The `name` column is a variable character string of up to 256 characters
 CREATE TABLE IF NOT EXISTS `id_not_null` (
-    `id` INT NOT NULL DEFAULT 1 PRIMARY KEY,
+    `id` INT NOT NULL DEFAULT 1,
     `name` VARCHAR(256)
 );
+
