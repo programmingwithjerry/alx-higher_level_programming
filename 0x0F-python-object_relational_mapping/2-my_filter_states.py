2fa15b941557aeaaa450d5e261b0023650d35902
programmingwithjerry <eneyellowj@gmail.com>
2024-06-22 20:40:01 +0000

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" a script that takes in an argument and displays all
-    values in the states table of hbtn_0e_0_usa where name
-    matches the argument.
+"""takes in an argument and displays all values
+   in the states table of hbtn_0e_0_usa
+   where name matches the argument
 """
 
 if __name__ == '__main__':
@@ -13,8 +13,8 @@ if __name__ == '__main__':
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name
-                LIKE BINARY 'N%' ORDER BY states.id ASC""")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
+                ORDER BY states.id ASC".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
