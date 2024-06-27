30198b4428fb03d35f571f273c82e81d92333b95
programmingwithjerry <eneyellowj@gmail.com>
2024-06-27 18:38:57 +0000

    13-model_state_delete_a.py

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Update a state
+    a script that deletes all State objects with a name
+    containing the letter a from the database hbtn_0e_6_usa
 """
 from sys import argv
 from model_state import Base, State
