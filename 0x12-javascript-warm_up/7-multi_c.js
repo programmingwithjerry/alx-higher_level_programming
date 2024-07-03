a59015df5ff5f8304e6f6db83173ba625af0ba74
programmingwithjerry <eneyellowj@gmail.com>
2024-07-03 13:52:00 -0700

    7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  const x = Number(process.argv[2]);
+  let index = 0;
+
+  while (index < x) {
+    console.log('C is fun');
+    index++;
+  }
+}
