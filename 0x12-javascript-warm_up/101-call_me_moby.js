444e63ed25d2c881805fa6ae60e369ab57344c69
programmingwithjerry <eneyellowj@gmail.com>
2024-07-03 14:50:33 -0700

    101-call_me_moby.js

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-// Export the callMeMoby function as a module
 exports.callMeMoby = function (x, theFunction) {
-  // Loop x times
   while (x-- > 0) {
-    // Call the provided function
     theFunction();
   }
 };
