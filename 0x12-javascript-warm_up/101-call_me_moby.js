9ae77271332f309e3a461ed76dde0e1a7199d7a0
programmingwithjerry <eneyellowj@gmail.com>
2024-07-03 14:46:25 -0700

    101-call_me_moby.js

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-// This line indicates that the script should be
-// run using the Node.js interpreter
-
 // Export the callMeMoby function as a module
 exports.callMeMoby = function (x, theFunction) {
   // Loop x times
