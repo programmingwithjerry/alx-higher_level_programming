7fbc60111c7690b79586a6685ff736633d58d910
programmingwithjerry <eneyellowj@gmail.com>
2024-07-05 05:52:39 -0700

    4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Script that fetches https://alx-intranet.hbtn.io/status and displays
+the body of the response.
+
+The response is displayed in the following format:
+    Body response:
+        - type: <class 'str'>
+        - content: OK
+"""
+import requests
+
+def fetch_status(url):
+    """
+    Fetches the URL and prints the body of the response.
+
+    Args:
+        url (str): The URL to fetch.
+    """
+    response = requests.get(url)
+    body = response.text
+
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+
+# Main entry point of the script
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    fetch_status(url)
+
