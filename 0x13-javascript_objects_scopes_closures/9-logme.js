10a0e5daf4a8922d561bab6be142a0d18fca5fdc
programmingwithjerry <eneyellowj@gmail.com>
2024-07-07 13:08:14 -0700

    9-logme.js

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+exports.logMe = function(item) {
+  // Initialize a static variable to keep track of the number of arguments printed
+  if (!exports.logMe.counter) {
+    exports.logMe.counter = 0;
+  }
+  
+  // Print the output in the specified format
+  console.log(`${exports.logMe.counter}: ${item}`);
+  
+  // Increment the counter for the next call
+  exports.logMe.counter++;
+};
