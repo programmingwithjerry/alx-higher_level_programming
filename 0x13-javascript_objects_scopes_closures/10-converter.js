ede40434830051e168e82ee8f1ef1097e35c132b
programmingwithjerry <eneyellowj@gmail.com>
2024-07-07 13:24:27 -0700

    10-converter.js

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,12 +1,4 @@
 #!/usr/bin/node
-exports.converter = function(base) {
-  // Define a recursive function to convert a number to the specified base
-  function convertToBase(num) {
-    if (num >= base) {
-      convertToBase(Math.floor(num / base)); // Recursive call with the quotient
-    }
-    console.log(num % base); // Print the remainder (digit in new base)
-  }
-
-  return convertToBase;
+exports.converter = function (base) {
+  return (number) => number.toString(base);
 };
