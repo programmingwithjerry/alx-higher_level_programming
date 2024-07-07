3bc6322e6aa1ac0de69cdd5a7c0338fc4264ead2
programmingwithjerry <eneyellowj@gmail.com>
2024-07-07 08:06:17 -0700

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-   Python script that fetches the 10 most recent commits from a
-   given GitHub repository,
-   displaying the SHA and author name of each commit.
+Python script that fetches the 10 most recent commits from a given GitHub repository,
+displaying the SHA and author name of each commit.
+
+Usage: ./commits_history.py <repository name> <owner name>
 """
 import requests
 from sys import argv
@@ -11,18 +12,16 @@ from sys import argv
 if __name__ == '__main__':
     # The repository name is passed as the first command-line argument
     repo = argv[1]
-    
-    # The owner/username of the repository is passed as the second
-    #command-line argument
-    name = argv[2]
+
+    # The owner/username of the repository is passed as the second command-line argument
+    owner = argv[2]
 
     # Define parameters to limit the number of commits fetched to 10
     params = {'per_page': 10}
 
-    # Send a GET request to the GitHub API to fetch commits of the
-    #specified repository
-    response = requests.get('https://api.github.com/repos/{}/{}/commits'
-            .format(name, repo), params=params)
+    # Send a GET request to the GitHub API to fetch commits of the specified repository
+    response = requests.get(f'https://api.github.com/repos/{owner}/{repo}/commits',\
+            params=params)
 
     # Parse the response as JSON
     commits = response.json()
@@ -31,4 +30,4 @@ if __name__ == '__main__':
     for commit in commits:
         sha = commit.get('sha')
         author_name = commit.get('commit').get('author').get('name')
-        print("{}: {}".format(sha, author_name))
+        print(f"{sha}: {author_name}")
