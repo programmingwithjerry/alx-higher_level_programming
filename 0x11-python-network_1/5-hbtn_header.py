6ca110139010d090825b217ff4e72e294610fd40
programmingwithjerry <eneyellowj@gmail.com>
2024-07-07 06:35:29 -0700

    5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
 """
-Script that takes in a URL, sends a request to the URL, and displays
+Python script that takes in a URL, sends a request to the URL, and displays
 the value of the X-Request-Id variable found in the response header.
-
-Usage: ./script.py <URL>
 """
 import requests
-import sys
+from sys import argv
 
 
-def fetch_x_request_id(url):
-    """
-    Sends a request to the URL and prints the value of the
-    X-Request-Id header.
-
-    Args:
-        url (str): The URL to send the request to.
-    """
-    response = requests.get(url)
-    x_request_id = response.headers.get('X-Request-Id')
-    print(x_request_id)
-
-
-# Main entry point of the script
 if __name__ == "__main__":
     # The URL to be fetched is passed as the first command line argument
-    url = sys.argv[1]
-    
-    # Call the function to fetch and print the 'X-Request-Id'
-    fetch_x_request_id(url)
+    url = argv[1]
+
+    try:
+        # Send a GET request to the provided URL
+        response = requests.get(url)
+        
+        # Print the value of the 'X-Request-Id' header if it exists
+        print(response.headers['X-Request-Id'])
+    except KeyError:
+        # If the 'X-Request-Id' header is not found, do nothing
+        pass
