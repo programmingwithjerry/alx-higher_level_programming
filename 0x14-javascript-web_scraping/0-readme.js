718e906f58d0a674280b3887fe78c71942b4ba04
programmingwithjerry <eneyellowj@gmail.com>
2024-07-30 16:50:04 +0100

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,13 @@
 const fs = require('fs');
 
 // Read the content of the file specified by the first command-line argument using 'utf8' encoding
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  // Print the error object if an error occurred, otherwise print the file content
-  console.log(error || content);
+fs.readFile(process.argv[2], 'utf8', function (err, contents) {
+  // Check if 'contents' is undefined, indicating an error occurred during the file read
+  if (contents === undefined) {
+    // Print the error object to the console
+    console.log(err);
+  } else {
+    // Print the file content to the console
+    console.log(contents);
+  }
 });
