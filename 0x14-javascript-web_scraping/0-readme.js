0215b93711f6cfba11bfb0fe32b694ab84fca2fa
programmingwithjerry <eneyellowj@gmail.com>
2024-07-30 16:45:13 +0100

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 
+// Import the 'fs' module to handle file system operations
 const fs = require('fs');
 
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf8', (error, data) => {
-  if (data === undefined) {
-    console.error(error);
-  } else {
-    console.log(data);
-  }
+// Read the content of the file specified by the first command-line argument using 'utf8' encoding
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  // Print the error object if an error occurred, otherwise print the file content
+  console.log(error || content);
 });
