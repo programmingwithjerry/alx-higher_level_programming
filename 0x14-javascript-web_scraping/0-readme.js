e40efb1df85c305e80c14ff9c6608c0330edd6e5
programmingwithjerry <eneyellowj@gmail.com>
2024-07-30 16:42:36 +0100

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,16 +1,13 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-const path = process.argv[2];
 
-if (!path) {
-	    console.error('Please provide a file path as the first argument.');
-	    process.exit(1);
-}
+const filePath = process.argv[2];
 
-fs.readFile(path, 'utf8', (err, data) => {
-	    if (err) {
-		            console.error('An error occurred:', err);
-		            return;
-		        }
-	    console.log(data);
+fs.readFile(filePath, 'utf8', (error, data) => {
+  if (data === undefined) {
+    console.error(error);
+  } else {
+    console.log(data);
+  }
 });
