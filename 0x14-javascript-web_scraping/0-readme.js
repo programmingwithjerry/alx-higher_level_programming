8e54a8b71c689560595b777082e1f06dccc06419
programmingwithjerry <eneyellowj@gmail.com>
2024-07-30 21:51:08 +0100

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -14,9 +14,14 @@
 //    console.log(contents);
 //  }
 //});
-
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
-  if (err) console.log(err);
-  else console.log(data);
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
